6fc3af27ac7d70c2e2083e7a5e5b25e82fd276b5
Alexander Kern <kern.alex@berkeley.edu>
2019-01-05 01:32:01 -0800

    saving for the sake of, fixed occasion, played with fetch_data

--- fetch_data.py
+++ fetch_data.py
@@ -10,14 +10,38 @@ def get_intraday_raw(name, date):
     :param name: ticker
     :param date: datetime.datetime.(year, month, day)
     :return: list of raw data
-    >>> 0 == 0
-    True
+    >>> import datetime
+    >>> get_intraday_raw("SPY", datetime.datetime(2019, 1, 3))[0]['date']
+    '20190103'
     """
     return iex.stocks.get_historical_intraday(name, date)
-def get_intraday_average_price(name, date):
+
+def intraday_volume(name, date):
+    """
+    :param name: ticker
+    :param date: datetime.datetime.(year, month, day)
+    :return: list of volumes for a given min
+    >>> from datetime import datetime
+    >>> intraday_volume("SPY", datetime(2019, 1, 3))[0]
+    3759
+    """
+    return [i['volume'] for i in get_intraday_raw(name, date)]
+
+
+def intraday_average_price(name, date):
+    """
+    :return: list of average prices per minute
+    >>> from datetime import datetime
+    >>> intraday_average_price("SPY", datetime(2019, 1, 3))[0]
+    248.407
+    """
     return [i['average'] for i in get_intraday_raw(name, date)]
+
+
 def get_five_year(name):
+
     return None
+
 
 if __name__=="__main__":
     import doctest
--- occasion.py
+++ occasion.py
@@ -4,10 +4,26 @@ ONLY file that uses datetime. tip your waitress
 
 import datetime
 
+
 def today():
-    return None
+    """
+    :return: IDK test it yourself
+    >>> today()
+    datetime.datetime(2019, 1, 5, 0, 0)
+    """
+    today = datetime.datetime.today()
+    return datetime.datetime(today.year, today.month, today.day)
+
+
 def five_years():
     now = today()
     return None
+
+
 def ninedy_days():
     return None
+
+
+if __name__=="__main__":
+    import doctest
+    doctest.testmod()
