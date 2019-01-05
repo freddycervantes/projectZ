f84b01ea3667cc67cbafff3b0e74624b5af025dd
Alexander Kern <kern.alex@berkeley.edu>
2019-01-04 23:47:50 -0800

    calander was taken, and added to fetch_data

--- fetch_data.py
+++ fetch_data.py
@@ -2,10 +2,23 @@
 File for getting data from IEX finance
 This will be the ONLY file where we import iexfinance
 """
-import iexfinance
+import iexfinance as iex
 
 
-def get_intraday(name, date):
-    return None
+def get_intraday_raw(name, date):
+    """
+    :param name: ticker
+    :param date: datetime.datetime.(year, month, day)
+    :return: list of raw data
+    >>> 0 == 0
+    True
+    """
+    return iex.stocks.get_historical_intraday(name, date)
+def get_intraday_average_price(name, date):
+    return [i['average'] for i in get_intraday_raw(name, date)]
 def get_five_year(name):
     return None
+
+if __name__=="__main__":
+    import doctest
+    doctest.testmod()
