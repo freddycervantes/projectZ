16c7b1157e1b2c4e1703f7215cdbb0ff63bfb833
Alexander Kern <kern.alex@berkeley.edu>
2019-01-05 16:13:13 -0800

    Updated occasion.py including doctests

--- fetch_data.py
+++ fetch_data.py
@@ -3,10 +3,19 @@ File for getting data from IEX finance
 This will be the ONLY file where we import iexfinance
 """
 import iexfinance as iex
+import occasion
+class Fetch:
 
+    def __init__(self):
+        self.__iex = self.__get_iex()
+
+    def __get_iex(self):
+        import iexfinance
+        return iexfinance
 
 def get_intraday_raw(name, date):
     """
+    need to update price every 3 months
     :param name: ticker
     :param date: datetime.datetime.(year, month, day)
     :return: list of raw data
@@ -16,8 +25,10 @@ def get_intraday_raw(name, date):
     """
     return iex.stocks.get_historical_intraday(name, date)
 
+
 def intraday_volume(name, date):
     """
+    need to update tests every 3 months
     :param name: ticker
     :param date: datetime.datetime.(year, month, day)
     :return: list of volumes for a given min
@@ -30,6 +41,7 @@ def intraday_volume(name, date):
 
 def intraday_average_price(name, date):
     """
+    need to update tests every 3 months
     :return: list of average prices per minute
     >>> from datetime import datetime
     >>> intraday_average_price("SPY", datetime(2019, 1, 3))[0]
@@ -38,9 +50,16 @@ def intraday_average_price(name, date):
     return [i['average'] for i in get_intraday_raw(name, date)]
 
 
-def get_five_year(name):
-
-    return None
+def get_five_year_raw(name):
+    """
+    HTF DO YOU TEST THIS??!!
+    :param name:
+    :return:
+    >>> 0==0
+    True
+    """
+    start, end = occasion.five_years()
+    return iex.stocks.get_historical_data(name, start, end)
 
 
 if __name__=="__main__":
--- occasion.py
+++ occasion.py
@@ -4,24 +4,60 @@ ONLY file that uses datetime. tip your waitress
 
 import datetime
 
+class Occasion:
 
-def today():
-    """
-    :return: IDK test it yourself
-    >>> today()
-    datetime.datetime(2019, 1, 5, 0, 0)
-    """
-    today = datetime.datetime.today()
-    return datetime.datetime(today.year, today.month, today.day)
+    def __init__(self):
+        self.__date = self.__import_datetime()
+        self.__day_gen = self.__date.datetime
+
+    def __import_datetime(self):
+        import datetime
+        return datetime
+
+    def get_date(self, year, month, day):
+        """
+        :return: datetime.datetime(year, month, day)
+        >>> Occasion().get_date(1987, 7, 28)
+        datetime.datetime(1987, 7, 28, 0, 0)
+        """
+        return self.__day_gen(year, month, day)
 
 
-def five_years():
-    now = today()
-    return None
+    def today(self):
+        """
+        :return: IDK test it yourself
+        >>> Occasion().today().year
+        2019
+        """
+        today = self.__day_gen.today()
+        return self.get_date(today.year, today.month, today.day)
 
+    def five_years(self):
+        """
+        :return: date of today and a day 5 years ago
+        >>> x = Occasion()
+        >>> x = x.five_years()
+        >>> x[0].year
+        2014
+        >>> x[1].year
+        2019
+        """
+        end = self.today()
+        start = end - self.__date.timedelta(days=(365 * 5))
+        return start, end
 
-def ninedy_days():
-    return None
+    def ninedy_days(self):
+        """
+        :return: list of days acending
+        >>> y = Occasion()
+        >>> x = y.ninedy_days()
+        >>> x[0].year*10000 + x[0].month*100 + x[0].day <  x[89].year*10000 + x[89].month*100 + x[89].day
+        True
+        >>> y.today() in x
+        True
+        """
+        today = self.today()
+        return list(reversed([today - self.__date.timedelta(days=i) for i in range(90)]))
 
 
 if __name__=="__main__":
